2f12ca661f4cba12ae1661a0e8e2c72a5ec3f9d5
Lady9696 <leidysuarez55@gmail.com>
2023-10-11 10:03:27 -0500

    okay

--- src/components-react/tapLink/index.jsx
+++ src/components-react/tapLink/index.jsx
@@ -10,14 +10,17 @@ const TapLink = ({ options = [] }) => {
     <section>
       {options?.map((option) => (
         <article key={option.name}>
-          <button key={option.id} onClick={handleSetLink(option.name)} className="bg-slate-500">
+          <button id={option.id} onClick={handleSetLink(option.name)} className="bg-slate-500">
             {option.name}
           </button>
           <div className={`flex flex-col gap-4 transition-all bg-blue-900 rounded-sm overflow-hidden  ${activeLink===option.name ?'max-h-[1000px] delay-200  ease-in-out duration-400':'max-h-0 duration-200'}` }>
             {option?.sublinks.map((sublink) => (
+              <button id={sublink.id}>
               <a href={sublink.link} key={sublink.name}>
-                {sublink.name}
+               {sublink.name}
+                
               </a>
+              </button>
             ))}
           </div>
         </article>
