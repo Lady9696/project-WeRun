213876ed7b853ae4f36b33e52627918570b24500
Lady9696 <leidysuarez55@gmail.com>
2023-10-11 09:55:38 -0500

    adding links and info

--- src/components-react/tapLink/index.jsx
+++ src/components-react/tapLink/index.jsx
@@ -1,20 +1,16 @@
 import { useState } from "react";
-// aquì coloco  que las props serà un array de opciones
 const TapLink = ({ options = [] }) => {
-  // establezco los el estado en null
   const [activeLink, setActiveLink] = useState(null);
-  //cuando el estado sea name , y le de click se va a actualizar a null(se esconde) , y cuando sea null se va actualizar a name (se muestre). 
   const handleSetLink = (name) => ()=> {
     setActiveLink(prev=>prev===name?null:name)
-    
     
 };
   console.log(activeLink, 'aqui');
   return (
-    <div>
+    <section>
       {options?.map((option) => (
         <article key={option.name}>
-          <button onClick={handleSetLink(option.name)} className="bg-slate-500">
+          <button key={option.id} onClick={handleSetLink(option.name)} className="bg-slate-500">
             {option.name}
           </button>
           <div className={`flex flex-col gap-4 transition-all bg-blue-900 rounded-sm overflow-hidden  ${activeLink===option.name ?'max-h-[1000px] delay-200  ease-in-out duration-400':'max-h-0 duration-200'}` }>
@@ -26,7 +22,7 @@ const TapLink = ({ options = [] }) => {
           </div>
         </article>
       ))}
-    </div>
+    </section>
   );
 };
 export default TapLink;
--- src/config/list.js
+++ src/config/list.js
@@ -32,148 +32,154 @@ export const ICONS = [
     link: "https://www.youtube.com/@ProyectoWeRun",
   },
 ];
-//Aqui coloco toda la info
-
 
 export const LINKSTORE = [
+  {
+    id: "bogota",
+    name: "BOGOTÁ",
+    src: "wpplogo.svg",
+
+    sublinks: [
+      {
+        id: "calle82",
+        name: "Nike Store Calle 82",
+        src: "wpplogo.svg",
+        link: "https://www.google.com/maps/place/Nike+Store/@4.6668873,-74.0546586,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9af5449e6bad:0xead6767acb510c3f!8m2!3d4.6668873!4d-74.0546586!16s%2Fg%2F1td77mk9?coh=164777&entry=tt&shorturl=1",
+      },
+      {
+        id: "parque-carolina",
+        name: "Nike Store Parque Colina",
+        src: "wpplogo.svg",
+        link: "https://www.google.com/maps/place/Nike+Store+Gran+Estaci%C3%B3n/@4.6484197,-74.103695,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9b112070e0fd:0x786bcacb8d9d1f70!8m2!3d4.6484144!4d-74.1015063!16s%2Fg%2F11h5l74lml?coh=164777&entry=tt&shorturl=1",
+      },
+      {
+        id: "parque-gran-estacion",
+        name: "Nike Store Gran Estación",
+        src: "wpplogo.svg",
+        link: "https://www.google.com/maps/place/Nike+Store+Gran+Estaci%C3%B3n/@4.6484197,-74.103695,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9b112070e0fd:0x786bcacb8d9d1f70!8m2!3d4.6484144!4d-74.1015063!16s%2Fg%2F11h5l74lml?coh=164777&entry=tt&shorturl=1",
+      },
+      {
+        id: "santafe",
+        name: "Nike Store C.C Santafé",
+        src: "wpplogo.svg",
+        link: "https://www.google.com/maps/place/Nike+Store/@4.7624616,-74.0487249,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f8593ed8ebd33:0xb16af0207ce76c38!8m2!3d4.7624563!4d-74.0465362!16s%2Fg%2F1tg5d4gb?coh=164777&entry=tt&shorturl=1",
+      },
+      {
+        id: "multiplaza",
+        name: "Nike Store Multiplaza",
+        src: "wpplogo.svg",
+        link: "https://www.google.com/maps/place/Nike+Store/@4.6525187,-74.1274235,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9d76d96dbd3b:0xf7dd79102bcbc157!8m2!3d4.6525187!4d-74.1274235!16s%2Fg%2F11f6561w1t?entry=ttu",
+      },
+      {
+        id: "plaza-central",
+        name: "Nike Store Plaza Central",
+        src: "wpplogo.svg",
+        link: "https://www.google.com/maps/place/Nike+Store/@4.6316371,-74.1172675,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f8558ffceef4d:0xea708ea3a2510364!8m2!3d4.6316318!4d-74.1150788!16s%2Fg%2F11j3h48qrw?coh=164777&entry=tt&shorturl=1",
+      },
+      {
+        id: "fontanar-chia",
+        name: "Nike Store Fontanar Chía",
+        src: "wpplogo.svg",
+        link: "https://www.google.com/maps/place/Nike+Store/@4.8834489,-74.0391732,17z/data=!3m1!4b1!4m6!3m5!1s0x8e40781f2ed00073:0x21fa4cb54de46331!8m2!3d4.8834436!4d-74.034946!16s%2Fg%2F11c2y70hxz?coh=164777&entry=tt&shorturl=1",
+      },
+    ],
+  },
 
   {
-    name: "Nike Store Bogotá",
+    id: "medellin",
+    name: " MEDELLÍN",
     src: "wpplogo.svg",
-    link: "https://api.whatsapp.com/send?phone=573212092394&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-    arrow: "arrow.svg",
+
     sublinks: [
       {
-        name: "Nike Store Medellín",
+        id: "viva-envigado",
+        name: "Nike Store Viva Envigado",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike+Store/@6.1760514,-75.5937808,17z/data=!3m1!4b1!4m6!3m5!1s0x8e468360efce7433:0x954a4d805098f4a!8m2!3d6.1760461!4d-75.5915921!16s%2Fg%2F11h8jbs8m4?coh=164777&entry=tt&shorturl=1",
       },
       {
-        name: "Nike Store Medellín",
+        id: "tesoro",
+        name: "Nike Store C.C. El Tesoro",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike+Store+El+Tesoro/@6.1976721,-75.5648601,17z/data=!3m1!4b1!4m6!3m5!1s0x8e46829a6cfb410b:0x7e88e9ccde4253cd!8m2!3d6.1976668!4d-75.5606329!16s%2Fg%2F11bycjjrrr?coh=164777&entry=tt&shorturl=1",
       },
       {
-        name: "Nike Store Medellín",
+        id: "cc-santafe",
+        name: "Nike Store C.C. Santafé",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike+Rise+Santaf%C3%A9+Medellin/@6.196193,-75.5760048,17z/data=!3m1!4b1!4m6!3m5!1s0x8e468262aaf4a90f:0xd0857d48fd45a415!8m2!3d6.1961877!4d-75.5738161!16s%2Fg%2F1pty5b8qz?coh=164777&entry=tt&shorturl=1",
       },
       {
-        name: "Nike Store Medellín",
+        id: "premium-plaza",
+        name: "Nike Store Premium Plaza",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/NIKE+SPORTLINE+%E2%80%A2+Premium+Plaza/@6.2299377,-75.5744134,17z/data=!3m1!4b1!4m6!3m5!1s0x8e44284a4847d65f:0x2fcd24b74da03d5a!8m2!3d6.2299324!4d-75.5701862!16s%2Fg%2F1pv5x5y6y?coh=164777&entry=tt&shorturl=1",
       },
     ],
   },
   {
-    name: "Nike Store Bogotá",
+    id: "cali",
+    name: "CALI",
     src: "wpplogo.svg",
-    link: "https://api.whatsapp.com/send?phone=573212092394&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-    arrow: "arrow.svg",
+
     sublinks: [
       {
-        name: "Nike Store Medellín",
+        id: "unicentro",
+        name: "Nike Store Unicentro",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike+Store/@3.3728486,-76.5415673,16.04z/data=!4m6!3m5!1s0x8e30a1d6c278777b:0xe85c15e025ac3c91!8m2!3d3.3732527!4d-76.5390951!16s%2Fg%2F11j10_mzyn?coh=164777&entry=tt&shorturl=1",
       },
       {
-        name: "Nike Store Medellín",
+        id: "jardin-plaza",
+        name: "Nike Jardín Plaza",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike+Store/@3.3692357,-76.5297074,17z/data=!3m1!4b1!4m6!3m5!1s0x8e30a174967b4303:0x195396fa9a4d3798!8m2!3d3.3692303!4d-76.5275187!16s%2Fg%2F11g6_wlzp5?coh=164777&entry=tt&shorturl=1",
       },
       {
-        name: "Nike Store Medellín",
+        id: "chipichape",
+        name: "Nike Store Chipichape",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike+Store+Chipichape/@3.4760898,-76.5335418,17z/data=!3m1!4b1!4m6!3m5!1s0x8e30a6190f80a6fd:0xf51485f33ed28d3e!8m2!3d3.4760845!4d-76.5293146!16s%2Fg%2F1q5bm3l2s?coh=164777&entry=tt&shorturl=1",
       },
     ],
   },
   {
-    name: "Nike Store Bogotá",
+    id: "BARRANQUILLA",
+    name: "BARRANQUILLA",
     src: "wpplogo.svg",
-    link: "https://api.whatsapp.com/send?phone=573212092394&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-    arrow: "arrow.svg",
+
     sublinks: [
       {
-        name: "Nike Store Medellín",
+        name: "Nike Store Buenavista",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike+Buena+Vista/@11.0138939,-74.8616554,14z/data=!4m7!3m6!1s0x8ef42c4d3f53a77d:0xb3757d47d8b05c6a!8m2!3d11.0138939!4d-74.8286964!15sCiBDZW50cm8gQ29tZXJjaWFsIEJ1ZW5hdmlzdGEgbmlrZSIDiAEBWiIiIGNlbnRybyBjb21lcmNpYWwgYnVlbmF2aXN0YSBuaWtlkgEQc3BvcnRzd2Vhcl9zdG9yZeABAA!16s%2Fg%2F1q5gmj01w?coh=164777&entry=tt&shorturl=1",
       },
     ],
   },
   {
-    name: "Nike Store Bogotá",
+    id: "manizales",
+    name: "MANIZALES",
     src: "wpplogo.svg",
-    link: "https://api.whatsapp.com/send?phone=573212092394&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-    arrow: "arrow.svg",
+
     sublinks: [
       {
-        name: "Nike Store Medellín",
+        id: "mallplaza",
+        name: "Nike Store Mall plaza",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike+%7C+Mallplaza+Manizales/@5.0653871,-75.4912383,17.76z/data=!4m6!3m5!1s0x8e476fd4edba6f49:0xf302489201f11594!8m2!3d5.0656644!4d-75.4901187!16s%2Fg%2F11f8m6c368?coh=164777&entry=tt&shorturl=1",
       },
     ],
   },
   {
-    name: "Nike Store Bogotá",
+    id: "pereira",
+    name: "PEREIRA",
     src: "wpplogo.svg",
-    link: "https://api.whatsapp.com/send?phone=573212092394&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-    arrow: "arrow.svg",
+
     sublinks: [
       {
-        name: "Nike Store Medellín",
+        name: "Nike Store Arboleda",
         src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
-      },
-      {
-        name: "Nike Store Medellín",
-        src: "wpplogo.svg",
-        link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
+        link: "https://www.google.com/maps/place/Nike/@4.8075818,-75.6880754,17z/data=!3m1!4b1!4m6!3m5!1s0x8e38873bf583a685:0x9cba41c209640369!8m2!3d4.8075765!4d-75.6838482!16s%2Fg%2F1tfjrrqs?coh=164777&entry=tt&shorturl=1",
       },
     ],
   },
