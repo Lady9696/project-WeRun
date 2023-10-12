a53dc0c61ac14b58a7febe42788aacada3017e94
Lady9696 <leidysuarez55@gmail.com>
2023-10-12 10:15:10 -0500

    adding hover

--- src/components-react/tapLink/index.jsx
+++ src/components-react/tapLink/index.jsx
@@ -17,7 +17,7 @@ const TapLink = ({ options = [] }) => {
           </button>
           <div className={`flex flex-col gap-4 transition-all  rounded-sm overflow-hidden  ${activeLink===option.name ?'max-h-[1000px] delay-200 py-4 ease-in-out duration-400':'max-h-0 duration-200'}` }>
             {option?.sublinks.map((sublink) => (
-              <button id={sublink.id} key={sublink.name} className="flex justify-center items-center bg-secondary py-2 px-6 rounded-full gap-2" > <img src={sublink.src}/>
+              <button id={sublink.id} key={sublink.name} className="flex justify-center items-center bg-secondary hover:bg-opacity-90 transition-colors duration-300 ease-in-out py-2 px-6 rounded-full gap-2" > <img src={sublink.src}/>
               <a href={sublink.link} target="_blank" rel="noopener noreferrer " className="font-PalatinoNova text-[18px] lg:text-[24px]" > 
                 {sublink.name}
               </a>
